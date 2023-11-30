76fe2727717d4753e6e8cade76a62de4fe84c4cf
pjrclarke <pauljrclarke@gmail.com>
2023-11-30 16:09:37 +0000

    all auth sent

--- event/models.py
+++ event/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=200)
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
+class Attendee(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.user.username} - {self.event.title}"
--- event/views.py
+++ event/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Event, Attendee
+from .forms import EventForm
 
-# Create your views here.
+
+
+def event_list(request):
+    events = Event.objects.all()
+    return render(request, 'event/event_list.html', {'events': events})
+def event_detail(request, event_id):
+    event = get_object_or_404(Event, pk=event_id)
+    attendees = Attendee.objects.filter(event=event)
+    return render(request, 'event/event_detail.html', {'event': event, 'attendees': attendees})
+@login_required
+def create_event(request):
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            event = form.save(commit=False)
+            event.organizer = request.user
+            event.save()
+            return redirect('event_detail', event_id=event.id)
+    else:
+        form = EventForm()
+    return render(request, 'event/event_form.html', {'form': form})
+@login_required
+def register_event(request, event_id):
+    event = get_object_or_404(Event, pk=event_id)
+    Attendee.objects.get_or_create(user=request.user, event=event)
+    return redirect('event_detail', event_id=event.id)
