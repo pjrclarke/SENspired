8f44294ae2ca5a21e00b9743bfa9ca74e6c9931d
pjrclarke <pauljrclarke@gmail.com>
2023-12-26 17:47:01 +0000

    degub turned off

--- senspired/settings.py
+++ senspired/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['senspired-efbab7dd64be.herokuapp.com', 'senspired.herokuapp.com','localhost','127.0.0.1']
 
